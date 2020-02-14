9b0df61e5a3afff0e74ad6ac4e49958e7aa82123
usergithub <breen.78@hotmail.com>
2020-02-14 12:30:59 -0600

    commit encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,8 @@
-
-    function cipher(word, offset){
-        var result="";
+const cipher = {
+  encode
+}
+    function encode (word, offset){
+        let resultEncode="";
       
       for (var i = 0; i < word.length ;  i++){
         let wordCipher = word.charCodeAt(i); // convierte la letra en numero ascii
@@ -9,12 +11,14 @@
          let formula = (wordCipher - 65 + offset) % 26 + 65; // aplica la formula a las letras
          let newLetter = String.fromCharCode(formula); //palabra cifrada
          
-         result += newLetter;
+         resultEncode += newLetter;
        }
       }
-      return result; //
+      return resultEncode; //
       
       }
+
+export default cipher
       
 
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
+import cipher from './cipher.js';
 
-    import cipher from './cipher.js';
+let palabra = " ";
+let mes = 0;
+let cifrar = document.getElementById('continuarCodigo');
 
-    let palabra = " ";
-    let mes = 0;
-    let cifrar = document.getElementById ('continuarCodigo');
-    
-    function obtenerDatos () {
-        palabra = document.getElementById('continuarCodigo');
-        mes = parseInt(document.getElementById('mesesCode');
-    }
+function obtenerDatos() {
+    palabra = document.getElementById('curpCode').value;
+    mes = parseInt(document.getElementById('mesesCode').value);
 
-    cifrar.onclick = function () {
-        obtenerDatos(palabra,mes);
-        document.getElementById('resultadoCifrado').value = cipher.cifrado (palabra,mes);
-        document.getElementById('resultadoCifrado').innerHTML = cipher.cifrado(palabra,mes);
-    }
+}
 
-    console.log(cipher);
-    
-    
+document.getElementById('continuarCodigo').onclick = function () {
+    console.log("click")
+    // 1.- necesitamos el texto y el numero
+    let texto = document.getElementById('curpCode').value
+    let offset = parseInt(document.getElementById('mesesCode').value)
+    // 2.- usamos cipher para convertir
+    let result = cipher.encode(texto, offset)
+    console.log("apostamos a que devuelve vacio?", result)
+    //3.- mostramos resultado en el html
+    document.getElementById('resultado').innerText = result
+};
+//document.getElementById('resultadoCifrado').value = cipher.cifrado(palabra, mes);
+console.log(cipher);
+
+
 
 
 
