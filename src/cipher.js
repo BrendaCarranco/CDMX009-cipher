5c47404341bd6163c8284b836ef8e51069486b21
usergithub <breen.78@hotmail.com>
2020-04-16 17:02:48 -0500

    cambios finales

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,5 @@
 const cipher = {
-  encode: (word, offset) => {
+  encode: (offset, word) => {
     let result = '';
     for (let i = 0; i < word.length; i++) {                        
       let wordCipher = word.charCodeAt(i);                      
@@ -8,23 +8,27 @@ const cipher = {
       } else
         if (wordCipher >= 97 && wordCipher <= 122) {
           wordCipher = word.charCodeAt(i);
-          result += String.fromCharCode((wordCipher - 97 + offset) % 26 + 65);
-          console.log(result)
+          result += String.fromCharCode((wordCipher - 97 + offset) % 26 + 97);
+          
         }
     }
     return result;
   },
 
-    decode: (word, offset) => {
-      let result = ''
-      for (let i = 0; i < word.length; i++) {
-        let wordCipher = word.charCodeAt(i);
-
-        if (wordCipher >= 65 && wordCipher <= 90) {
-          let formula = (wordCipher - 65 - offset) % 26 + 65;
-          return result;
+  decode : (offset, word) => {
+    let result = '';
+    for (let i = 0; i < word.length; i++) {                        
+      let wordCipher = word.charCodeAt(i);                      
+      if (wordCipher >= 65 && wordCipher <= 90) {                  
+        result += String.fromCharCode((wordCipher - 65 - offset) % 26 + 65);
+      } else
+        if (wordCipher >= 97 && wordCipher <= 122) {
+          wordCipher = word.charCodeAt(i);
+          result += String.fromCharCode((wordCipher - 97 - offset) % 26 + 97);
+          
         }
-      }
     }
-  }
+    return result;
+  },
+}
 export default cipher
--- src/index.js
+++ src/index.js
@@ -1,39 +1,31 @@
 import cipher from './cipher.js';
 
-continuarCode.onclick = function () {
+let continueCode = document.getElementById('continueCode');
+let continueUser = document.getElementById('continueUser');
+
+continueCode.onclick = function () {
     let word = document.getElementById('curpCode').value;
     let offset = parseInt(document.getElementById('mesesCode').value);
-    let result = cipher.encode(word, offset);
-    console.log(result);
-    document.getElementById('resultado').innerHTML = 'Tú código es ' + result;
+    let result = cipher.encode(offset, word);
+    document.getElementById('resultado').innerHTML = 'Tú código es:  ' + result;
 };
 
-continuarUser.onclick = function () {                       //deshabilitar
+continueUser.onclick = function () {
     let word = document.getElementById('curpUser').value;
-    let offset = parseInt(document.getElementById('mesesUser').value);
-    let result = cipher.decode(word, offset);
-    console.log(result);
-    document.getElementById('resultadoDes').innerHTML = 'Tú código es: ' + result;
+    let offset = document.getElementById('mesesUser').value;
+    let result = cipher.decode(offset,word);
+    document.getElementById('resultadoDes').innerHTML = 'Tu usuario es: ' + result;
 };
 
+let btnCode = document.getElementById('noCode');
+let btnUser = document.getElementById('noUser');
 
+btnCode.onclick = function () {
+    document.getElementById('p1').classList.add('hide');
+    document.getElementById('p2').classList.remove('hide')
+}
 
-
-
-
-
-/*document.getElementById('continuarCode').onclick = function () {
-
-    // 1.- necesitamos el texto y el numero
-    let word = document.getElementById('curpCode').value;
-    let offset = parseInt(document.getElementById('mesesCode').value);
-    // 2.- usamos cipher para convertir
-    let result = cipher.encode(word, offset);
-    console.log(result); // (mostramos en consola para verificar que haga la f{})
-    //3.- mostramos resultado en el html
-    document.getElementById('resultado').innerText = ('Tu código es: ' + result);
-}; */
-
-
-
-console.log(cipher);
+btnUser.onclick = function () {
+    document.getElementById('p1').classList.add('hide');
+    document.getElementById('p3').classList.remove('hide');
+}
